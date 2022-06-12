dc25391720f5c1c0ffbd6a09d6afba8919f169e0
rohit <rohitsinghind22@gmail.com>
2022-06-12 19:28:01 +0530

    bug fix

--- src/components/userlist/index.jsx
+++ src/components/userlist/index.jsx
@@ -39,7 +39,7 @@ useEffect(() => {
                 }
                 <Box >
                     <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                        {users?.data.map((e) => (
+                        {users?.data?.map((e) => (
                         <Grid item xs={2} sm={4} md={4} key={e.id}>
                             <Paper sx={styles.paper} elevation={2}>
                                 <div>
